2175ce2fd32251e110101f5a3cf0b3f4cf644748
Alejandro Perdomo González <apergot95@gmail.com>
2020-03-16 09:42:55 +0000

    spring initializr project

---
+++ src/test/java/com/apergot/springbootrestapi/SpringBootRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apergot.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
