857b967a3349a8e57ae73e299af1e5152629004f
Alejandro Perdomo González <apergot95@gmail.com>
2020-03-16 11:53:16 +0000

    endpoint creation to get all clients and test if idid it right

---
+++ src/main/java/com/apergot/springbootrestapi/controllers/ClientRestController.java
@@ -0,0 +1,23 @@
+package com.apergot.springbootrestapi.controllers;
+
+import com.apergot.springbootrestapi.models.entity.Client;
+import com.apergot.springbootrestapi.models.services.InterfaceClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ClientRestController {
+
+    @Autowired
+    private InterfaceClientService clientService;
+
+    @GetMapping("/clients")
+    public List<Client> index() {
+        return clientService.findAll();
+    }
+}
