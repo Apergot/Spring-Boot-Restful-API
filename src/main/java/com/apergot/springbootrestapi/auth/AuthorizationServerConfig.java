6da2b33a6e7eae9c6b6210a8e09c931efe76c9fd
Alejandro Perdomo González <apergot95@gmail.com>
2020-04-08 13:20:06 +0100

    Adding rsa keys and using them to create and verify authentication tokens

--- src/main/java/com/apergot/springbootrestapi/auth/AuthorizationServerConfig.java
+++ src/main/java/com/apergot/springbootrestapi/auth/AuthorizationServerConfig.java
@@ -58,6 +58,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(JwtConfig.PRIVATE_RSA);
+        jwtAccessTokenConverter.setVerifierKey(JwtConfig.PUBLIC_RSA);
         return jwtAccessTokenConverter;
     }
 }
