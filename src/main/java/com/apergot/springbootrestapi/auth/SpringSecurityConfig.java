84fb89e985d347006f3aa97d359ef895efb4df85
Alejandro Perdomo González <apergot95@gmail.com>
2020-04-08 11:54:36 +0100

    Adding route protection from OAuth side and disabling csrf on spring side because we are not using sessions as spring mvc

--- src/main/java/com/apergot/springbootrestapi/auth/SpringSecurityConfig.java
+++ src/main/java/com/apergot/springbootrestapi/auth/SpringSecurityConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,5 +32,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //Spring side routes security
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //Disabling session management because we are using spring as an API and not as Spring MVC
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
